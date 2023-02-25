f74b9d280154c37f05863472efb19e05771c870c
whimsicalwill <knipewilliam20@gmail.com>
2023-02-25 18:13:15 -0500

    Remove minerl dependencies temporarily

--- pydreamer/envs/__init__.py
+++ pydreamer/envs/__init__.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore", ".*Box bound precision lowered by casting")  #
 import gym
 import numpy as np
 
-from .import minecraft_minerl
+# from .import minecraft_minerl
 from .wrappers import *
 
 
@@ -60,9 +60,9 @@ def create_env(env_id: str, no_terminal: bool, env_time_limit: int, env_action_r
         # can add recorder wrapper here for videos
         env = DictWrapper(env) # other gym envs use this wrapper
 
-    elif env_id.startswith('minecraft'):
-        env = minecraft_minerl.MineRLEnv().make()
-        env = DictWrapper(env) # other gym envs use this wrapper
+    # elif env_id.startswith('minecraft'):
+    #     env = minecraft_minerl.MineRLEnv().make()
+    #     env = DictWrapper(env) # other gym envs use this wrapper
 
     else:
         env = gym.make(env_id)
